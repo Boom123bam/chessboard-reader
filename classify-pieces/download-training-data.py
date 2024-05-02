4a70aae526e10800d8fb1b951b4e94b7c41248a5
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-05-02 13:05:45 +0100

    download and train

--- classify-pieces/download-training-data.py
+++
@@ -1,8 +0,0 @@
-import roboflow
-
-roboflow.login()
-
-rf = roboflow.Roboflow()
-project = rf.workspace("chess-detector").project("chess-predictor")
-version = project.version(10)
-dataset = version.download("folder")
