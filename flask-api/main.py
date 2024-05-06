5259f35618af0ed20be33b238207c7c442887015
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-05-06 07:56:08 +0100

    return JSON

--- flask-api/main.py
+++ flask-api/main.py
@@ -1,7 +1,14 @@
 from findchessboard import find_chessboard
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import numpy as np
 import cv2
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 app = Flask(__name__)
 
@@ -9,16 +16,32 @@ app = Flask(__name__)
 @app.route("/getchessboard", methods=["GET", "POST"])
 def get_chessboard():
     if request.method == "POST":
-        # Get the image file
-        image_file = request.files["file"]
-        # Read the image file using cv2
-        image_array = np.frombuffer(image_file.read(), np.uint8)
-        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-        [found, a, b] = find_chessboard(image)
-        if found:
-            return str(a) + str(b)
+        if "file" not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files["file"]
+
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            return jsonify({"error": "No selected file"}), 400
+        if file and allowed_file(file.filename):
+            # Get the image file
+            image_file = request.files["file"]
+            # Read the image file using cv2
+            image_array = np.frombuffer(image_file.read(), np.uint8)
+            image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+            # Perform chessboard detection
+            found, a, b = find_chessboard(image)
+            if found:
+                return jsonify({"success": True, "a": a.tolist(), "b": b.tolist()}), 200
+            else:
+                return (
+                    jsonify({"success": False, "message": "No chessboard found"}),
+                    404,
+                )
         else:
-            return "No board"
+            return jsonify({"error": "File type not allowed"}), 400
+
     else:
         return """
         <!doctype html>
@@ -33,26 +56,3 @@ def get_chessboard():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# if __name__ == "__main__":
-#     # read grayscale
-#     # original = cv2.imread("test-images/pic.jpg", cv2.IMREAD_GRAYSCALE)
-#     # original = cv2.imread("test-images/mirror.jpg", cv2.IMREAD_GRAYSCALE)
-#     # original = cv2.imread("test-images/board.png", cv2.IMREAD_GRAYSCALE)
-#     # original = cv2.imread("test-images/respawn.png", cv2.IMREAD_GRAYSCALE)
-#     original = cv2.imread("test-images/respawn2.png", cv2.IMREAD_GRAYSCALE)
-#     # original = cv2.imread("test-images/board-light.png", cv2.IMREAD_GRAYSCALE)
-#     # original = cv2.imread("test-images/example.jpeg", cv2.IMREAD_GRAYSCALE)
-
-#     h, w = original.shape
-#     board_x_lines, board_y_lines = find_chessboard(original)
-
-#     plt.imshow(original, cmap="gray")
-#     for hx in board_x_lines:
-#         plt.axhline(round(hx * h), color="b", lw=2)
-
-#     for hy in board_y_lines:
-#         plt.axvline(round(hy * w), color="r", lw=2)
-
-#     plt.waitforbuttonpress()
