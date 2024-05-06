7422e5c076080e2754b71d815f6eb7c41d3e98c0
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-05-06 11:03:44 +0100

    API working

--- flask-api/processboard.py
+++ flask-api/processboard.py
@@ -1,6 +1,5 @@
 import cv2
 from ultralytics import YOLO
-from findchessboard import find_chessboard
 
 model = YOLO("flask-api/best.pt", task="predict", verbose=False)
 
@@ -12,6 +11,8 @@ def classifyPieces():
         id = r.probs.top1
         piece = r.names[id]
         pieces.append(piece)
+
+    assert len(pieces) == 64, "Number of images must be 64"
 
     return pieces
 
@@ -32,8 +33,28 @@ def splitBoard(boardImg, xLines, yLines):
             i += 1
 
 
+def generateFen(pieces):
+    result = ""
+    blanks = 0
+    for i, piece in enumerate(pieces):
+        if piece == "empty":
+            blanks += 1
+        else:
+            if blanks != 0:
+                result += str(blanks)
+                blanks = 0
+            result += piece[1] if piece[0] == "b" else piece[1].upper()
+        if (i + 1) % 8 == 0:
+            if blanks != 0:
+                result += str(blanks)
+                blanks = 0
+            result += "/"
+    return result
+
+
 # img = cv2.imread("classify-pieces/pieces/b-r.png")
 # img = cv2.imread("test-images/respawn.png")
 # ok, xl, yl = find_chessboard(img)
 # splitBoard(img, xl, yl)
-classifyPieces()
+pieces = classifyPieces()
+print(generateFen(pieces))
